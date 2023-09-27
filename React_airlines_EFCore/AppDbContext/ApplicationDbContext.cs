361ebc46f97abc4b0073df00837a498f02e67a00
Klaudia Agnieszka Matuszak <klaudia.matuszak99@gmail.com>
2023-09-27 21:06:27 +0200

    Add db

--- React_airlines_EFCore/AppDbContext/ApplicationDbContext.cs
+++ React_airlines_EFCore/AppDbContext/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using React_airlines_EFCore.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,16 @@ namespace React_airlines_EFCore.AppDbContext
 {
     public class ApplicationDbContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext() : base()
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test");
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
         }
     }
 }
